e6fd0f83ed6b14a874e703f62fcdcb13b6e8eddc
Jairo Serrano <jairo.serrano@gmail.com>
2021-05-25 11:40:33 -0500

    ajustes a los nombres

--- test/LoadBenchAgentRemote/benchmark_deploy.sh
+++ test/LoadBenchAgentRemote/benchmark_deploy.sh
@@ -27,11 +27,11 @@ scp -prq confbesa.xml sistemas@10.195.20.34:/home/sistemas/dist/
 scp -prq confbesa.xml sistemas@10.195.20.42:/home/sistemas/dist/
 
 echo "Copiando archivos execute_benchmark.py"
-scp -prq execute_benchmark.py sistemas@10.195.20.22:/home/sistemas/dist/
-scp -prq execute_benchmark.py sistemas@10.195.20.27:/home/sistemas/dist/
-scp -prq execute_benchmark.py sistemas@10.195.20.31:/home/sistemas/dist/
-scp -prq execute_benchmark.py sistemas@10.195.20.34:/home/sistemas/dist/
-scp -prq execute_benchmark.py sistemas@10.195.20.42:/home/sistemas/dist/
+scp -prq benchmark_execute.py sistemas@10.195.20.22:/home/sistemas/dist/
+scp -prq benchmark_execute.py sistemas@10.195.20.27:/home/sistemas/dist/
+scp -prq benchmark_execute.py sistemas@10.195.20.31:/home/sistemas/dist/
+scp -prq benchmark_execute.py sistemas@10.195.20.34:/home/sistemas/dist/
+scp -prq benchmark_execute.py sistemas@10.195.20.42:/home/sistemas/dist/
 
 echo "Copiando archivos data benchmark"
 scp -prq *.csv sistemas@10.195.20.22:/home/sistemas/dist/
