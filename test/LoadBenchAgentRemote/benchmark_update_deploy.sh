57f94380cfa8912b6f5bf67e6fa0f6279f686fd5
Jairo Serrano <jairo.serrano@gmail.com>
2021-09-20 09:29:02 -0500

    Tag monolitic

--- test/LoadBenchAgentRemote/benchmark_update_deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Copiando archivos ejecutables"
-scp -prq dist/ sistemas@10.195.20.22:/home/sistemas/
-scp -prq dist/ sistemas@10.195.20.27:/home/sistemas/
-scp -prq dist/ sistemas@10.195.20.31:/home/sistemas/
-scp -prq dist/ sistemas@10.195.20.34:/home/sistemas/
-scp -prq dist/ sistemas@10.195.20.42:/home/sistemas/
-
-echo "Copia terminada"
--- test/LoadBenchAgentRemote/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-java -Djava.library.path=/home/jairo/BESA/opencv/build/lib/ -cp dist/LoadBenchAgentRemote.jar ContainersLauncher.LoadContainer_$1  > $1.txt 2>&1
